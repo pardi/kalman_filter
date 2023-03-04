88c01a124be4d707a54fbb64e554a535e9630ceb
pardi <tommaso.pardi@gmail.com>
2023-03-04 22:28:21 +0000

    chore{}: comply with linting

--- main.py
+++ main.py
@@ -10,38 +10,53 @@ def main():
 
     X = []
     X_real = []
-    DX = []
+    # DX = []
     DX_real = []
     X_kf = []
     DX_kf = []
+    U = []
 
     kf = KalmanFilt(x0=np.array([[0.0], [0.0]]))
+    N = 100
+    flag = True
+    u = 0
 
-    for idx in range(100):
-        u = 10 * np.sin(2 * np.pi * idx / 100.0)
+    for idx in range(N):
+        t = idx / float(N)
+        # f = 10
 
-        x = my_sys.send_command(u)
+        if flag:
+            u = 10 * np.sin(2 * np.pi * t)
+            if u < 0:
+                flag = False
+
+        z = my_sys.send_command(u)
         x_real = my_sys.x
 
-        x_hat = kf.prediction()
+        x_kf = kf.prediction()
 
-        x_kf = kf.update(x, x_hat)
+        if idx % 3 == 0:
+            x_kf = kf.update(z)
 
-        X.append(x[0])
+        X.append(z[0])
         X_real.append(x_real[0])
         X_kf.append(x_kf[0])
-        DX.append(x[1])
+        # DX.append(x[1])
         DX_real.append(x_real[1])
         DX_kf.append(x_kf[1])
+        U.append(u)
 
-    fig, ax = plt.subplots(2)
-
+    fig, ax = plt.subplots(3)
+    #
     ax[0].plot(X, "b")
     ax[0].plot(X_real, "r")
-    ax[0].plot(X_kf, "-cx")
-    ax[1].plot(DX, "b")
+    ax[0].plot(X_kf, "-c")
+    # ax[1].plot(DX, "b")
     ax[1].plot(DX_real, "r")
-    ax[1].plot(DX_kf, "-cx")
+    ax[1].plot(DX_kf, "-c")
+
+    # ax[2].plot([x - y for x, y in zip(X, X_kf)], "b")
+    ax[2].plot([x - y for x, y in zip(DX_kf, DX_real)], "b")
     plt.show()
 
 
--- models.py
+++ models.py
@@ -8,8 +8,7 @@ class SimpleModel:
         self.noise_covariance = noise_covariance
         self.A = np.array([[0, 1], [0, 0]])
         self.b = np.array([[0], [1]])
-        self.c = np.array([[1, 0],
-                          [0, 1]])
+        self.c = np.array([[1, 0], [0, 1]])
         self.d = 0
         self.dt = dt
 
@@ -23,7 +22,7 @@ class SimpleModel:
         self.x = self.x + dx * self.dt
 
     def _get_measurements(self) -> np.array:
-        return self.c @ self.x + .2 * np.random.rand(1, 1) - 0.1
+        return self.c @ self.x + 0.2 * np.random.rand(1, 1) - 0.1
 
     def get_nominal_model(self) -> (np.array, np.array, np.array, np.array):
         return self.A, self.b, self.c, self.d
