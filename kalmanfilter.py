e6717104e809216be544ba8762f3c84cc6956129
pardi <tommaso.pardi@gmail.com>
2023-03-04 22:28:01 +0000

    feature{kalmanfilt}: fixed algorithm

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 class KalmanFilt:
-    def __init__(self, x0: np.array, dt: float = 0.001):
-        self.Q = np.array([[1, 0], [0, 1]])
+    def __init__(self, x0: np.array, dt: float = 0.01):
+        self.Q = np.array([[1, 1], [0, 1]])
         self.R = np.array([[1, 0], [0, 1]])
-        self.F = np.array([[5, 3], [0, 5]])
+        self.F = np.array([[1, 0], [0, 1]])
         self.B = np.array([[0], [1]])
-        self.H = np.array([[1, 0.0], [0.0, 1.0]])
+        self.H = np.array([[1.0, 0.0], [0.0, 1.0]])
 
         self.P = np.array([[1, 0], [0, 1]])
 
@@ -20,17 +20,18 @@ class KalmanFilt:
 
         self.dt = dt
 
-    def prediction(self):
-        x_hat = self.F @ self.x + self.B * self.u
+    def prediction(self) -> np.array:
+        self.x = self.F @ self.x + self.B * self.u
 
         self.P = self.F @ self.P @ self.F.T + self.Q
 
-        return x_hat
+        return self.x
 
-    def update(self, z: np.array, x_hat: np.array):
-        y = z - self.H @ x_hat
+    def update(self, z: np.array) -> np.array:
+        y = z - self.H @ self.x
         K = self.P @ self.H.T @ np.linalg.inv(self.R + self.H @ self.P @ self.H.T)
 
-        self.x = x_hat + K @ y
+        self.x = self.x + K @ y
         self.P = (np.identity(2) - K @ self.H) @ self.P
+
         return self.x
